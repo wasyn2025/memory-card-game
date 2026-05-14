259f9613887e3cf6aea607a61f50aaf848bc1582
wasyn2025 <wasynssulaiman@gmail.com>
2026-05-14 12:06:36 +0700

    Added correction logic for same card

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-let selectedCards = [];
+let selectedCard = [];
+let cardId = {
+    "xh401": "apple",
+    "xh402": "banana"
+};
 
-document.querySelectorAll("#memory-card[data-value]").forEach((card) => {
-    card.addEventListener("click", (event) => {
-        if(selectedCards.length === 2) {
-            checkMatch();
-            return;
+document.querySelectorAll("#card").forEach(element => {
+    element.addEventListener("click", (event) => {
+        if (selectedCard.length < 2) {
+            selectedCard.push(event.currentTarget.getAttribute("name"));
+            event.currentTarget.classList.add("rotate180");
+
+            if (selectedCard.length === 2) {
+                let card1 = cardId[selectedCard[0]];
+                let card2 = cardId[selectedCard[1]];
+                (card1 === card2) ? alert("Kartu sama! +5") : alert("Kartu berbeda! -5");
+
+                removeMatchedCard();
+            }
         }
-
-        selectedCards.push(card);
-    })
+    });
 });
 
-function checkMatch() {
+function resetFlippedCard() {
+    
+}
 
+function removeMatchedCard() {
+    selectedCard.length = [];
 }
