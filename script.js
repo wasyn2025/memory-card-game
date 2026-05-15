7535581f4186272b13d25a64c27e77edb4b82080
wasyn2025 <wasynssulaiman@gmail.com>
2026-05-15 11:00:28 +0700

    Add card generation based on data logic

--- script.js
+++ script.js
@@ -1,36 +1,83 @@
+document.addEventListener("DOMContentLoaded", () => {
+    generateCard();
+});
+
 let selectedCard = [];
-let cardId = {
-    "xh401": "apple",
-    "xh402": "banana",
-    "xh403": "apple",
-    "xh404": "banana"
+const cardData = {
+    "xh401": { "name": "apple", "emoji": "🍎" },
+    "xh402": { "name": "banana", "emoji": "🍌" },
+    "xh403": { "name": "grape", "emoji": "🍇" },
+    "xh404": { "name": "orange", "emoji": "🍊" },
+    "xh405": { "name": "watermelon", "emoji": "🍉" },
+    "xh406": { "name": "strawberry", "emoji": "🍓" },
+
+    // pasangan
+    "xh407": { "name": "apple", "emoji": "🍎" },
+    "xh408": { "name": "banana", "emoji": "🍌" },
+    "xh409": { "name": "grape", "emoji": "🍇" },
+    "xh410": { "name": "orange", "emoji": "🍊" },
+    "xh411": { "name": "watermelon", "emoji": "🍉" },
+    "xh412": { "name": "strawberry", "emoji": "🍓" },
 };
+let cardTotal = 12 / 2;
+const cardContainer = document.querySelector("#card-container");
 
-document.querySelectorAll("#card").forEach(element => {
-    element.addEventListener("click", (event) => {
-        let targetElement = event.currentTarget;
-        let attribute = targetElement.getAttribute("name");
+function generateCard() {
+    const cardDataEntries = Object.entries(cardData);
+    console.log(cardDataEntries);
+    cardContainer.innerHTML = '';
 
-        if (selectedCard.length < 2 && !selectedCard.includes(attribute)) {
-            selectedCard.push(attribute);
-            targetElement.classList.add("rotate180");
-            targetElement.classList.add("pointer-events-none");
+    for (let i = 0; i < 12; i++) {
+        const id = cardDataEntries[i][0];
+        const emoji = cardDataEntries[i][1].emoji;
 
-            if (selectedCard.length === 2) {
-                let card1 = cardId[selectedCard[0]];
-                let card2 = cardId[selectedCard[1]];
+        cardContainer.innerHTML += `<div class="card-wrapper transition-all duration-600">
+            <div class="h-full rounded-md group cursor-pointer border border-white p-3 bg-stone-800 flex items-center justify-center transition-all duration-600 hover:shadow-[0_0_15px_rgba(255,255,255,0.35)]"
+                id="card" name="${id}">
+                <div
+                    class="text-white text-5xl font-bold opacity-75 transition-opacity duration-200 group-hover:opacity-100">
+                    ?</div>
+                <div id="card-back-content" class="card-back text-white text-5xl rounded-md">${emoji}</div>
+            </div>
+        </div>`;
+    }
 
-                if (card1 === card2) {
-                    console.log("Kartu sama! +5");
-                    setTimeout(removeMatchedCard, 1000);
-                } else {
-                    console.log("Kartu berbeda! -5");
-                    setTimeout(resetFlippedCard, 1000);
+    document.querySelectorAll("#card").forEach(element => {
+        element.addEventListener("click", (event) => {
+            let targetElement = event.currentTarget;
+            let attribute = targetElement.getAttribute("name");
+
+            if (selectedCard.length < 2 && !selectedCard.includes(attribute)) {
+                selectedCard.push(attribute);
+                targetElement.classList.add("rotate180");
+                targetElement.classList.add("pointer-events-none");
+
+                if (selectedCard.length === 2) {
+                    const card1 = cardData[selectedCard[0]]?.name;
+                    const card2 = cardData[selectedCard[1]]?.name;
+
+                    if (card1 === card2) {
+                        console.log("Kartu sama! +5");
+                        cardTotal -= 1;
+                        console.log(cardTotal);
+                        setTimeout(removeMatchedCard, 1000);
+                    } else {
+                        console.log("Kartu berbeda!");
+                        setTimeout(resetFlippedCard, 1000);
+                    }
                 }
             }
-        }
+
+            if (cardTotal === 0) {
+                setTimeout(() => alert("Gameover!"), 1000);
+            }
+        });
     });
-});
+}
+
+function loadEmojiData() {
+
+}
 
 function resetFlippedCard() {
     document.querySelector(`[name="${selectedCard[0]}"]`).classList.remove("rotate180");
